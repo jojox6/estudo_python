0a387d97ab52dd57b81d3746c67f36c59781e379
Joseph Oliveira <33632611+jojox6@users.noreply.github.com>
2024-01-17 23:30:08 +0000

    up

---
+++ iniciante/aula11.py
@@ -0,0 +1,10 @@
+# IMC Exercício
+
+# o índice de massa corporal de um adulto é o seu peso em quilos 
+# (por exemplo, 80 kg), dividido por sua altura ao quadrado (vamos imaginar, 1,80 m x 1,80 m).
+
+nome = "Joseph Oliveira"
+peso = 105.00
+altura = 1.87
+imc = peso/(2**altura)
+print(f'Nome: {nome}, peso: {peso} seu IMC é {imc}')
